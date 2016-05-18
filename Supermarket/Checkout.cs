e35ebb81c37febf8bf126a45edde8f9536699360
Ben <Ben@CURLYBEN>
2016-05-18 12:44:18 +0100

    -

--- Supermarket/Checkout.cs
+++ Supermarket/Checkout.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Supermarket
@@ -17,6 +18,13 @@ namespace Supermarket
                 // line should look like: Book 20.99 for example
                 bill.Lines.Add(basket.Items[i].Price.ToString());
             }
+
+            bill.Lines.Add("");
+            bill.Lines.Add("");
+            bill.Lines.Add("");
+            bill.Lines.Add("");
+            bill.Lines.Add("");
+
             return bill;
         }
     }
